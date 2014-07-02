609fe6f1fb299acd9b3d6565b748ee1995d6c6ca
gaoyuqiao <gaoyuqiao@hotmail.com>
2014-07-02 13:06:43 +0800

    添加web services 支持

--- server/webServices.js
+++ server/webServices.js
@@ -8,21 +8,33 @@
 
 var fs = require('fs');
 
+
+var methods = {
+
+}
+
 function webServices(req, res) {
     var mime = {
         js: 'application/x-javascript',
         css: 'text/css',
-        html: 'text/html'
+        html: 'text/html',
+        json: 'application/x-javascript'
     }
 
-    var head = mime.js + ';charset=utf-8';
+    //http://xxx.xxx.xxx.xx/#ws/action/a=1&b=2
+    var head = mime.json + ';charset=utf-8';
     res.writeHead(200, {'Content-Type': head});
-    if (u != 'favicon.ico') {
-        fs.readFile(url.replace('/', ''), 'utf8', function (err, data) {
-            res.write(data);
-            res.end();
-        });
+    var url = req.url,
+        arr = url.split('/'),
+        len = arr.length,
+        method = arr[len - 2],
+        params = arr[len - 1];
+
+    switch (method) {
+
     }
+
+
 }
 
 

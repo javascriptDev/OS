c0671f0c11940903693b9411e3b1ead73b32b549
gaoyuqiao <gaoyuqiao@hotmail.com>
2014-07-17 19:03:59 +0800

    添加折线图

---
+++ client/util/js/lib/line.js
@@ -0,0 +1,18 @@
+/**
+ * Created by a2014 on 14-7-17.
+ */
+function Line(o) {
+    this.data = o.data;
+    this.x = o.x;
+    this.y = o.y;
+
+}
+
+Line.prototype = {
+    init: function () {
+
+    },
+    draw: function () {
+
+    }
+}

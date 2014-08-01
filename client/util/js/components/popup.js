2c04edfac1d86613f2f7d5575b1e8b1428839788
gaoyuqiao <gaoyuqiao@hotmail.com>
2014-08-01 18:50:38 +0800

    添加 popup 删除item 方法

--- client/util/js/components/popup.js
+++ client/util/js/components/popup.js
@@ -21,6 +21,13 @@ function popup(opt) {
     this.setData = function (data) {
         _data = data;
     }
+    this.delData = function (data) {
+        _data.list[0].data.forEach(function (d, index) {
+            if (d.text == data.text && d.value == data.value) {
+                _data.list[0].data.splice(index, 1);
+            }
+        })
+    }
     this.init();
 }
 
@@ -53,7 +60,6 @@ popup.prototype = {
         if (this.opt.isRender) {
             this.render();
         }
-
     },
     render: function () {
         var me = this;
@@ -66,6 +72,17 @@ popup.prototype = {
         this.listC = this.el.querySelector(this.opt.listContainer);
         this.show();
         me.opt.events.rendered && me.opt.events.rendered.call(this);
+        this.listC.onclick = function (e) {
+            var tar = e.target;
+            if (tar.className = 'pop-list-item-del') {
+                me.updataList({
+                    text: tar.offsetParent.childNodes[0].innerHTML,
+                    value: tar.offsetParent.childNodes[1].innerHTML
+                }, false, function () {
+                    tar.offsetParent.parentNode.removeChild(tar.offsetParent);
+                });
+            }
+        }
     },
     addButton: function () {
         var me = this, opt = me.opt, btn = opt.btn;
@@ -91,23 +108,29 @@ popup.prototype = {
         data && this.setData(data);
         isRender && this.render();
     },
-    updataList: function (data) {
-        this.addData(data);
-        this.listC.innerHTML += template.compile(this.opt.listTpl)({list: data});
+    updataList: function (data, addOrDel, fn) {//true is add . false is del
         var sum = 0;
-        data.forEach(function (val) {
-            sum += parseInt(val.value);
-        })
-        this.setSum(parseInt(this.getSum()) + sum);
+        if (addOrDel) {
+            this.addData(data);
+            this.listC.innerHTML += template.compile(this.opt.listTpl)({list: data});
+            data.forEach(function (val) {
+                sum += parseInt(val.value);
+            });
+            this.setSum(parseInt(this.getSum()) + sum);
+        } else {
+            this.delData(data);
+//            this.listC.innerHTML = this.listC.innerHTML.replace(template.compile(this.opt.listTpl)({list: [ data]}), '');
+            sum = this.getSum() - parseInt(data.value);
+            this.setSum(parseInt(sum));
+        }
         this.el.querySelector('.fred').childNodes[1].innerText = this.getSum();
+        fn && fn();
     },
-    hide: function () {
-//        this.c.style.display = 'none';
-        this.c.style.webkitTransform = 'scale(0)'
 
+    hide: function () {
+        this.c.style.webkitTransform = 'scale(0)'
     },
     show: function () {
-//        this.c.style.display = 'block';
         this.c.style.webkitTransform = 'scale(1)'
     },
     destroy: function () {

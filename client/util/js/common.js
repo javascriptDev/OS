29aa3db1474a59fab281f5e05818ade412590d36
Administrator <627535440@qq.com>
2014-08-02 16:54:11 +0800

    添加修改菜单 消息同步

--- client/util/js/common.js
+++ client/util/js/common.js
@@ -1,59 +1,67 @@
 /**
  * Created by a2014 on 14-7-25.
  */
-function getData(params, fn, needParse) {
-    request(msj.ip + ':8000/!ws?data/' + params, function (data) {
-        var param = data;
-        needParse && (param = parseData(param));
-        fn && fn({list: param || []});
-    })
+function getData( params, fn, needParse ) {
+	request( msj.ip + ':8000/!ws?data/' + params, function( data ) {
+		var param = data;
+		needParse && (param = parseData( param ));
+		fn && fn( {list: param || []} );
+	} )
 }
-function parseData(data) {
-    var result = [];
-    for (var i = 0, len = data.length; i < len; i++) {
+function parseData( data ) {
+	var result = [];
+	for( var i = 0, len = data.length; i < len; i++ ) {
 
-        var d = data[i];
-        var o = {did: 0, order: '', price: 0};
-        o.did = d.dn;
-        o.id = d._id;
-        o.statues = d.statues;
-        o.data = d.data.list;
-        var detail = d.data.list;
-        for (var j = 0, l = detail.length; j < l; j++) {
+		var d = data[i];
+		var o = {did: 0, order: '', price: 0};
+		o.did = d.dn;
+		o.id = d._id;
+		o.statues = d.statues;
+		o.data = d.data.list;
+		var detail = d.data.list;
+		for( var j = 0, l = detail.length; j < l; j++ ) {
 //            o.order += detail[j].text + ':' + parseInt(detail[j].value) + '\r\n,';
-            o.price += parseInt(detail[j].value);
-        }
-        result.push(o);
-    }
-    return result;
+			o.price += parseInt( detail[j].value );
+		}
+		result.push( o );
+	}
+	return result;
 }
-function request(url, cb) {
-    var xhr;
-    if (window.XMLHttpRequest) { // Mozilla, Safari,...
-        xhr = new XMLHttpRequest();
+function unparseData( data ) {
+	var o = {}
+	o.dn = data.did;
+	o.statues = data.statues;
+	o.data = {list: data.data};
+	o._id = data.id;
+	return o;
+}
+function request( url, cb ) {
+	var xhr;
+	if(window.XMLHttpRequest) { // Mozilla, Safari,...
+		xhr = new XMLHttpRequest();
 
-    } else if (window.ActiveXObject) { // IE
-        try {
-            xhr = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch (e) {
-            try {
-                xhr = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (e) {
-            }
-        }
-    }
+	} else if(window.ActiveXObject) { // IE
+		try {
+			xhr = new ActiveXObject( "Msxml2.XMLHTTP" );
+		} catch(e) {
+			try {
+				xhr = new ActiveXObject( "Microsoft.XMLHTTP" );
+			} catch(e) {
+			}
+		}
+	}
 
-    if (!xhr) {
-        alert('Giving up :( Cannot create an XMLHTTP instance');
-        return false;
-    }
-    xhr.open('get', url);
-    xhr.send();
-    xhr.onreadystatechange = function (e) {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            cb && cb(JSON.parse(xhr.responseText));
-        }
+	if(!xhr) {
+		alert( 'Giving up :( Cannot create an XMLHTTP instance' );
+		return false;
+	}
+	xhr.open( 'get', url );
+	xhr.send();
+	xhr.onreadystatechange = function( e ) {
+		if(xhr.readyState == 4 && xhr.status == 200) {
+			cb && cb( JSON.parse( xhr.responseText ) );
+		}
 
-    }
+	}
 }
 
--- client/util/js/util.js
+++ client/util/js/util.js
@@ -1,138 +1,138 @@
 /**
  * Created by addison on 14-6-23.
  */
-function inherit(subclass, superclass) {
-    var F = function () {
-        },
-        subclassProto = subclass.prototype,
-        superclassProto = superclass.prototype;
+function inherit( subclass, superclass ) {
+	var F = function() {
+		},
+		subclassProto = subclass.prototype,
+		superclassProto = superclass.prototype;
 
-    F.prototype = superclassProto;
-    subclass.prototype = new F();
-    subclass.prototype.constructor = subclass;
-    subclass.superclass = superclassProto;
+	F.prototype = superclassProto;
+	subclass.prototype = new F();
+	subclass.prototype.constructor = subclass;
+	subclass.superclass = superclassProto;
 
-    if (superclassProto.constructor === Object.constructor) {
-        superclassProto.constructor = superclass;
-    }
-    var subProtoMethod = Object.keys(subclassProto);
+	if(superclassProto.constructor === Object.constructor) {
+		superclassProto.constructor = superclass;
+	}
+	var subProtoMethod = Object.keys( subclassProto );
 
-    Array.prototype.forEach.call(subProtoMethod, function (item) {
-        if (subclassProto.hasOwnProperty(item)) {
-            if (Object.prototype.toString.call(subclassProto[item]) == '[object Function]') {
-                subclass.prototype[item] = subclassProto[item];
-            }
-        }
-    });
-    return subclass;
+	Array.prototype.forEach.call( subProtoMethod, function( item ) {
+		if(subclassProto.hasOwnProperty( item )) {
+			if(Object.prototype.toString.call( subclassProto[item] ) == '[object Function]') {
+				subclass.prototype[item] = subclassProto[item];
+			}
+		}
+	} );
+	return subclass;
 }
 
 msj = {
-    ip: 'http://192.168.165.166',
-//    ip: 'http://192.168.1.101',
+//    ip: 'http://192.168.165.166',
+	ip: 'http://192.168.1.101',
 
-    role: {
-        //显示设备
-        monitor: 'monitor',
-        //记账
-        base: 'base',
-        //终端，点菜器
-        terminal: 'terminal'
-    },
-    et: {
-        addDesk: 'addDesk',
-        login: 'login',
-        loginSuccess: 'ls',
-        makeOver: 'mo',
-        pay: 'pay'
+	role: {
+		//显示设备
+		monitor: 'monitor',
+		//记账
+		base: 'base',
+		//终端，点菜器
+		terminal: 'terminal'
+	},
+	et: {
+		addDesk: 'addDesk',
+		login: 'login',
+		loginSuccess: 'ls',
+		makeOver: 'mo',
+		pay: 'pay',
+		changeList: 'changeList'
+	},
+	tpl: {
+		a: "<%for (var i=0;i<list.length;i++) {%>" +
+			"<div class=list-item>" +
+			"<img class=food src='<%= list[i].img %>' />" +
+			"<div class=text><%=list[i].text%></div>" +
+			"<div class=price><%= list[i].price%></div>" +
+			"</div>" +
+			"<%}%>",
+		e: "<%for (var i=0;i<list.length;i++) {%>" +
+			"<div class=list-item>" +
+			"<div class=text><%=list[i].text%></div>" +
+			"<div class=price><%= list[i].price%></div>" +
+			"</div>" +
+			"<%}%>",
+		b: "<%for (var i=0;i<list.length;i++) {%>" +
+			"<div class='list-item ol-list-item'>" +
+			"<div class=ol-text><%=list[i].text%></div>" +
+			"<div class=ol-price><%=list[i].value%></div>" +
+			"<div class=ol-del data-id=<%=list[i].id%> >X</div>" +
+			"</div>" +
+			"<%}%>",
+		c: "<%for (var i=0;i<list.length;i++) {%>" +
+			"<div class='list-item'>" +
+			"<div class=text><%=list[i].text%></div>" +
+			"</div>" +
+			"<%}%>",
+		d: "<%for (var i=0;i<list.length;i++) {%>" +
+			"<div class='nestedlist-item'><%=list[i].text%></div>" +
+			"<%}%>",
+		popup: "<div class=pop-title>交易详情</div>" +
+			"<%for (var i=0;i<list.length;i++) {%>" +
 
-    },
-    tpl: {
-        a: "<%for (var i=0;i<list.length;i++) {%>" +
-            "<div class=list-item>" +
-            "<img class=food src='<%= list[i].img %>' />" +
-            "<div class=text><%=list[i].text%></div>" +
-            "<div class=price><%= list[i].price%></div>" +
-            "</div>" +
-            "<%}%>",
-        e: "<%for (var i=0;i<list.length;i++) {%>" +
-            "<div class=list-item>" +
-            "<div class=text><%=list[i].text%></div>" +
-            "<div class=price><%= list[i].price%></div>" +
-            "</div>" +
-            "<%}%>",
-        b: "<%for (var i=0;i<list.length;i++) {%>" +
-            "<div class='list-item ol-list-item'>" +
-            "<div class=ol-text><%=list[i].text%></div>" +
-            "<div class=ol-price><%=list[i].value%></div>" +
-            "<div class=ol-del data-id=<%=list[i].id%> >X</div>" +
-            "</div>" +
-            "<%}%>",
-        c: "<%for (var i=0;i<list.length;i++) {%>" +
-            "<div class='list-item'>" +
-            "<div class=text><%=list[i].text%></div>" +
-            "</div>" +
-            "<%}%>",
-        d: "<%for (var i=0;i<list.length;i++) {%>" +
-            "<div class='nestedlist-item'><%=list[i].text%></div>" +
-            "<%}%>",
-        popup: "<div class=pop-title>交易详情</div>" +
-            "<%for (var i=0;i<list.length;i++) {%>" +
+			"<div class=order-list-item-container>" +
+			"<div class=pop-list-item>桌号</div>" +
+			"<div class=pop-list-item><%=list[i].did%></div>" +
+			"</div>" +
 
-            "<div class=order-list-item-container>" +
-            "<div class=pop-list-item>桌号</div>" +
-            "<div class=pop-list-item><%=list[i].did%></div>" +
-            "</div>" +
+			"<div class=order-list>" +
+			"<div class='order-list-item-container list-field'>" +
+			"<div class=pop-list-item>菜名</div>" +
+			"<div class=pop-list-item>单价</div>" +
+			"<div class=pop-list-item>操作</div>" +
 
-            "<div class=order-list>" +
-            "<div class='order-list-item-container list-field'>" +
-            "<div class=pop-list-item>菜名</div>" +
-            "<div class=pop-list-item>单价</div>" +
-            "<div class=pop-list-item>操作</div>" +
+			"</div>" +
 
-            "</div>" +
+			"<div class='order-list-body'>" +
+			"<%for (var j=0;j<(list[i].data&&list[i].data.length);j++) {%>" +
+			"<div class=order-list-item-container>" +
+			"<div class=pop-list-item><%= list[i].data[j].text%></div>" +
+			"<div class=pop-list-item><%= list[i].data[j].value%></div>" +
+			"<div class=pop-list-item-del>X</div>" +
+			"</div>" +
+			"<%}%>" +
+			"</div>" +
+			"</div>" +
 
-            "<div class='order-list-body'>"+
-            "<%for (var j=0;j<(list[i].data&&list[i].data.length);j++) {%>" +
-            "<div class=order-list-item-container>" +
-            "<div class=pop-list-item><%= list[i].data[j].text%></div>" +
-            "<div class=pop-list-item><%= list[i].data[j].value%></div>" +
-            "<div class=pop-list-item-del>X</div>" +
-            "</div>" +
-            "<%}%>" +
-            "</div>"+
-            "</div>" +
-
-            "<div class='order-list-item-container fred'>" +
-            "<div class=pop-list-item>总价</div>" +
-            "<div class='pop-list-item '><%=list[i].price%></div>" +
-            "</div>" +
-            "<div class=btn-c></div>" +
-            "</div>" +
-            "<%}%>",
-        popList: "<%for (var j=0;j<list.length;j++) {%>" +
-            "<div class=order-list-item-container>" +
-            "<div class=pop-list-item><%= list[j].text%></div>" +
-            "<div class=pop-list-item><%= list[j].value%></div>" +
-            "<div class=pop-list-item-del>X</div>" +
-            "</div>" +
-            "<%}%>"
-    },
-    createEl: function (domName, cfg) {
-        var el = document.createElement(domName);
-        msj.recursion(el, cfg);
-        return el;
-    },
-    //递归函数
-    recursion: function (dom, o) {
-        for (var i in o) {
-            if (Object.prototype.toString.call(o[i]) == '[object Object]') {
-                msj.recursion(dom[i], o[i]);
-            } else {
-                dom[i] = o[i];
-            }
-        }
-    }
+			"<div class='order-list-item-container fred'>" +
+			"<div class=pop-list-item>总价</div>" +
+			"<div class='pop-list-item '><%=list[i].price%></div>" +
+			"</div>" +
+			"<div class=btn-c></div>" +
+			"</div>" +
+			"<%}%>",
+		popList: "<%for (var j=0;j<list.length;j++) {%>" +
+			"<div class=order-list-item-container>" +
+			"<div class=pop-list-item><%= list[j].text%></div>" +
+			"<div class=pop-list-item><%= list[j].value%></div>" +
+			"<div class=pop-list-item-del>X</div>" +
+			"</div>" +
+			"<%}%>"
+	},
+	createEl: function( domName, cfg ) {
+		var el = document.createElement( domName );
+		msj.recursion( el, cfg );
+		return el;
+	},
+	//递归函数
+	recursion: function( dom, o ) {
+		for( var i in o ) {
+			if(Object.prototype.toString.call( o[i] ) == '[object Object]') {
+				msj.recursion( dom[i], o[i] );
+			} else {
+				dom[i] = o[i];
+			}
+		}
+	}
 
 
 
